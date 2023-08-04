09cd32e81664aa411508f15975af7a15dcfceba0
Abdullah-encrypts <Dev.AbdullahAwan@gmail.com>
2023-08-05 00:06:49 +0500

    added markdown data in place of dummy data in featured and all posts pages

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import AllPosts from '../../components/posts/all-posts'
+import { getAllPosts } from '../../lib/posts-util'
 
-const AllPostsPage = () => {
+const AllPostsPage = (props) => {
+
   return (
-    <div>AllPostsPage</div>
+    <AllPosts posts={props.posts}/>
   )
 }
+
+export function getStaticProps(){
+    const allPosts = getAllPosts();
+
+    return {
+      props: {
+        posts: allPosts
+      }
+    }
+}
+
 
 export default AllPostsPage
---
+++ posts/getting-started-with-nextjs.md
@@ -0,0 +1,11 @@
+---
+title: 'Getting started with NextJS'
+date: '2022-10-15'
+image: getting-started-nextjs.png
+excerpt: "NextJS is a React js framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR."
+isFeatured: true
+---
+
+# This is a title
+
+This is some regular text with some [link](https://google.com)
